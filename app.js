27bfa9062424c7576e15756cd0ec267bd59fd0de
Laura Silvey <lsilvey@gmail.com>
2017-11-15 16:41:37 -0800

    added dependencies

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
 
+//change 'boiler-plate' to name of app.
+mongoose.connect("mongodb://localhost/boiler-plate", {useMongoClient: true});
 app.set("view engine", "ejs");
+//to access custom stylesheets
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res){
   res.render("home");
